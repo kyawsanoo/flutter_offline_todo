c382f14aa73f508820a46c0b6747b66f81bfa3e8
kyawsanoo <kyawsanoo.androider@gmail.com>
2024-01-14 19:52:06 +0630

    initialized

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:todoapp_offline/TodoListScreen.dart';
 import 'package:todoapp_offline/models/Todo.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  //final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
-  //Hive.init(appDocumentDir.path);
   await Hive.initFlutter();
   await Hive.openBox<Todo>("todobox");
   Hive.registerAdapter(TodoAdapter());
